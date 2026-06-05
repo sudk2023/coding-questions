0c3f54c89c52b696e3b8688e4a18bf34f50577d9
sudk2023 <129952430+sudk2023@users.noreply.github.com>
2026-06-05 10:01:13 +0000

    update

---
+++ Basic Programs/1_CheckEvenOdd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    int number = 0;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0 ) {
+        printf("The number is Even\n");   
+    } else {
+        printf("The number is Odd \n");
+    }
+
+    return 0;
+}
